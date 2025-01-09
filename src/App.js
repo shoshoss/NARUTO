f9bd03cccb9d911c8f10b2091e03f42444c61bc2
shota <s8152.45@gmail.com>
2025-01-09 09:29:34 +0900

    Add: Axiosを使ってReactからAPIをコールした

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		fetchCharacters();
+	}, []);
+
+	const fetchCharacters = async () => {
+		const apiUrl = "https://narutodb.xyz/api/character";
+
+		const result = await axios.get(apiUrl);
+		console.log(result);
+	};
+	return <div className="App">helo world</div>;
 }
 
 export default App;
